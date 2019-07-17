d1d75375c3f972f20f70e42fafe544205d1facfa
yutong821 <1993821liu@gmail.com>
2019-07-17 14:25:42 -0700

    finsih lab2

--- lab02/Account.java
+++ lab02/Account.java
@@ -4,12 +4,34 @@
  */
 public class Account {
 
+    public static void main(String[] args) {
+        System.out.println("liu yutong shi gou");
+        Account christine = new Account(500);
+        Account matt = new Account(100, christine);
+        int test = matt.balance;
+        System.out.println(test);
+        matt.withdraw(50);
+        int test2 = matt.balance;
+        System.out.println(test2);
+        matt.withdraw(300);
+        System.out.println(christine.balance);
+    }
+
     public int balance;
+    public Account parentAccount;
 
     /** Initialize an account with the given balance. */
     public Account(int balance) {
         this.balance = balance;
+        this.parentAccount = null;
     }
+
+    // add two argument Account
+    public Account(int balance, Account parentAccount) {
+        this.balance = balance;
+        this.parentAccount = parentAccount;
+    }
+
 
     /** Deposits amount into the current account. */
     public void deposit(int amount) {
@@ -25,15 +47,27 @@ public class Account {
      * would leave a negative balance, print an error message and leave the
      * balance unchanged.
      */
-    public void withdraw(int amount) {
+    public boolean withdraw(int amount) {
         // TODO
         if (amount < 0) {
             System.out.println("Cannot withdraw negative amount.");
+            return false;
         } else if (balance < amount) {
-            System.out.println("Insufficient funds");
+            // System.out.println("Insufficient funds");
+            if (this.parentAccount != null && amount - this.balance < this.parentAccount.balance) {
+                this.parentAccount.balance = this.parentAccount.balance - amount + this.balance;
+                this.balance = 0;
+                return true;
+            } else if (this.parentAccount != null && amount - this.balance > this.parentAccount.balance) {
+                System.out.println("Insufficient funds");
+                return false;
+            }else {
+                return false;
+            }
         } else {
-            balance -= amount;
-        }
+            this.balance -= amount;
+            return true;
+        } 
     }
 
     /**
@@ -41,6 +75,7 @@ public class Account {
      * and depositing it into this account.
      */
     public void merge(Account other) {
-        // TODO
+        this.deposit(other.balance);
+        other.withdraw(other.balance);
     }
 }
--- lab02/Path.java
+++ lab02/Path.java
@@ -1,6 +1,73 @@
 /** A class that represents a path via pursuit curves. */
+
 public class Path {
 
     // TODO
+    private Point currPoint;
+    private Point nextPoint;
 
+
+    public Path(double x, double y){
+    	// this.x = x;
+    	// this.y = y;
+    	this.currPoint = new Point(x, y);
+    	this.nextPoint = new Point(x, y);
+    }
+
+    public double getCurrX(){
+    	return this.currPoint.getX();
+    }
+
+    public double getCurrY(){
+    	return this.currPoint.getY();
+    }
+
+    public double getNextX(){
+    	return this.nextPoint.getX();
+    }
+
+    public double getNextY(){
+    	return this.nextPoint.getY();
+    }
+
+    public Point getCurrentPoint(){
+    	return currPoint;
+    }
+
+    public void setCurrentPoint(Point point){
+    	this.currPoint.setX(point.getX());
+    	this.currPoint.setY(point.getY());
+    }
+
+    public void iterate(double dx, double dy){
+    	this.currPoint = nextPoint;
+    	this.nextPoint.setX(currPoint.getX() + dx);
+    	this.nextPoint.setY(currPoint.getY() + dy);
+    }
 }
+
+// /** A class that represents a path via pursuit curves.
+//  *  @author You!
+//  */
+// public class Path {
+
+//     /** What to do, what to do... */
+// 	private Point currPoint;
+//     private Point nextPoint;
+
+// 	public Path(double x, double y){
+//         currPoint = new Point(x, y);
+//         nextPoint = new Point(x, y);
+// 	}
+//     public double getCurrX(){return this.currPoint.getX();}
+//     public double getCurrY(){return this.currPoint.getY();}
+//     public double getNextX(){return this.nextPoint.getX();}
+//     public double getNextY(){return this.nextPoint.getY();}
+//     public void iterate(double dx, double dy){
+//         currPoint.setX(nextPoint.getX());
+//         currPoint.setY(nextPoint.getY());
+//         nextPoint.setX(currPoint.getX()+dx);
+//         nextPoint.setY(currPoint.getY()+dy);
+//     }
+
+// }
